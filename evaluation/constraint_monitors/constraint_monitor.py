439b6754ae17d472c991770d0ef49c36c6a191ca
gtouya <guillaume.touya@ign.fr>
2022-03-04 15:53:47 +0100

    first packages of the project

---
+++ evaluation/constraint_monitors/constraint_monitor.py
@@ -0,0 +1,8 @@
+from shapely.geometry import Point
+
+class ConstraintMonitor:
+    def get_geometry(self, path: str, file_name: str) -> Point:
+        """Load in the file for extracting text."""
+        pass
+
+
