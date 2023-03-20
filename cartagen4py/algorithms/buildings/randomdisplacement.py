e0de8cd5aff8d26aa88c7c66b0ae4807f9450816
jberli <justinberli@gmail.com>
2023-03-20 09:28:59 +0100

    added verbose mode to random displacement

--- cartagen4py/algorithms/buildings/randomdisplacement.py
+++ cartagen4py/algorithms/buildings/randomdisplacement.py
@@ -6,25 +6,28 @@ class BuildingDisplacementRandom:
     """
         Initialize random displacement object, with default length displacement factor and number of iterations per building
     """
-    def __init__(self, max_trials=15, max_displacement=10):
+    def __init__(self, max_trials=15, max_displacement=10, verbose=False):
         self.MAX_TRIALS = max_trials
         self.MAX_DISPLACEMENT = max_displacement
+        self.VERBOSE = verbose
 
     def displace(self, buildings, roads, rivers):
         # Calculating the mean rate of overlapping buildings with othe buildings, roads and rivers
         # It represents the mean congestion of buildings within the building block
-        print("Launching random displacement.")
-        print("buildings: " + str(len(buildings)))
-        print("roads: " + str(len(roads)))
-        print("rivers: " + str(len(rivers)))
-        print("calculating rate mean...")
+        if (self.VERBOSE):
+            print("Launching random displacement.")
+            print("buildings: " + str(len(buildings)))
+            print("roads: " + str(len(roads)))
+            print("rivers: " + str(len(rivers)))
+            print("calculating rate mean...")
         rate_mean = self.__get_buildings_overlapping_rate_mean(buildings, roads, rivers)
-        print("rate mean: " + str(rate_mean))
 
         # Starting trial count
         trial = 0
         # Launching the loop which will displace buildings randomly as long as the rate mean is above 0 and the max trial count is not exceeded
         while rate_mean > 0 and trial <= self.MAX_TRIALS:
+            if (self.VERBOSE):
+                print("TRIAL " + str(trial))
             # Selecting a random building index
             random_index = random.randint(0, len(buildings) - 1)
             random_building = buildings[random_index]
@@ -53,7 +56,6 @@ class BuildingDisplacementRandom:
                 else:
                     rate_mean = new_rate_mean
                     trial = 0
-                print(rate_mean)
                 
         return buildings
 
@@ -67,11 +69,16 @@ class BuildingDisplacementRandom:
 
         # For each building, calculating the area overlapping other buildings, roads and rivers depending on a distance value
         for b in buildings:
+            if (self.VERBOSE):
+                print("building " + str(nb))
             mean += self.__get_building_overlap(b, buildings, roads, rivers)
             nb += 1
         # Calculating the mean area
         if nb != 0:
             mean = mean/nb
+
+        if (self.VERBOSE):
+            print("rate mean: " + str(rate_mean))
 
         return mean
 
@@ -83,15 +90,15 @@ class BuildingDisplacementRandom:
         for building in buildings:
             # Checking if it's not the same building
             if building != processed_building:
-                geometry = self.__get_overlapping_geometries(processed_building, building, geometry)
+                geometry = self.__get_overlapping_geometries(processed_building, building, geometry, "building")
         
         # For each road section
         for road in roads:
-            geometry = self.__get_overlapping_geometries(processed_building, road, geometry)
+            geometry = self.__get_overlapping_geometries(processed_building, road, geometry, "road")
 
         # For each river section
         for river in rivers:
-            geometry = self.__get_overlapping_geometries(processed_building, river, geometry)
+            geometry = self.__get_overlapping_geometries(processed_building, river, geometry, "river")
 
         # Returning the area of the geometry if it exists
         if (geometry is None) or (geometry.is_empty == True) or (geometry.area == 0):
@@ -100,9 +107,11 @@ class BuildingDisplacementRandom:
             return geometry.area
 
     # Calculate the geometry of the intersection between a geographic object and a building if the building if it exists
-    def __get_overlapping_geometries(self, processed_building, obj, geometry):
+    def __get_overlapping_geometries(self, processed_building, obj, geometry, type):
         # If the building intersects the object
-        if shapely.intersects(processed_building, obj):
+        if shapely.overlaps(processed_building, obj):
+            if (self.VERBOSE):
+                print("intersecting " + type)
             # Creating the intersection between the building and the object
             intersection = shapely.intersection(processed_building, obj)
             # If the geometry is empty, return the intersection...
