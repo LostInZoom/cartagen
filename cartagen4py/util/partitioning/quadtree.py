d85d55760e09bbabe4c987a46495229abdc5ed1c
jberli <justinberli@gmail.com>
2023-03-24 11:24:53 +0100

    started quadtree

---
+++ cartagen4py/util/partitioning/quadtree.py
@@ -0,0 +1,15 @@
+import pyqtree, shapely
+
+class QuadTree:
+    """
+        Create a quad tree object
+    """
+    def __init__(self, objects, max_objects, max_depth):
+        self.OBJECTS = objects
+        self.MAX_OBJECTS = max_objects
+        self.MAX_DEPTH = max_depth
+
+        self.EXTENT = shapely.bounds(objects).tolist()
+
+    def subdivide():
+        pass
