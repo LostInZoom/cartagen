a9af12c4a9d60ce0e5548796d150791832290010
jberli <justinberli@gmail.com>
2023-03-24 11:23:51 +0100

    random displacement now takes geopandas df

--- cartagen4py/algorithms/buildings/randomdisplacement.py
+++ cartagen4py/algorithms/buildings/randomdisplacement.py
@@ -14,9 +14,10 @@ class BuildingDisplacementRandom:
         self.VERBOSE = verbose
 
     def displace(self, buildings, roads, rivers, *networks):
+        self.__BUILDINGS = buildings
         if (self.VERBOSE):
             print("Launching random displacement.")
-            print("buildings: " + str(len(buildings)))
+            print("buildings: " + str(len(self.__BUILDINGS)))
             print("roads: " + str(len(roads)))
             print("rivers: " + str(len(rivers)))
 
@@ -24,30 +25,30 @@ class BuildingDisplacementRandom:
             if self.VERBOSE:
                 print("Creating the network partition.")
             # Overwrite buildings with partitionned buildings
-            partitions = network_partition(buildings, *networks)
+            partitions = network_partition(self.__BUILDINGS, *networks)
             total_length = len(partitions[0])
             if self.VERBOSE:
                 print("{0} partitions created.".format(total_length))
 
-            buildings = []
             for i, partition in enumerate(partitions[0]):
-                if self.VERBOSE:
-                    print("Treating partition {0}/{1}".format(i + 1, total_length))
-                    print("    {0} buildings".format(len(partition)))
+                if i < 20:
+                    if self.VERBOSE:
+                        print("Treating partition {0}/{1}".format(i + 1, total_length))
+                        print("    {0} buildings".format(len(partition)))
 
-                partition_roads = []
-                for road in roads:
-                    if road.intersects(partitions[1][i]):
-                        partition_roads.append(shapely.intersection(road, partitions[1][i]))
-                partition_rivers = []
-                for river in rivers:
-                    if river.intersects(partitions[1][i]):
-                        partition_rivers.append(shapely.intersection(river, partitions[1][i]))
-                buildings = buildings + self.__random_displacement(partition, partition_roads, partition_rivers)
+                    partition_roads = []
+                    for road in roads.geometry:
+                        if road.intersects(partitions[1][i]):
+                            partition_roads.append(shapely.intersection(road, partitions[1][i]))
+                    partition_rivers = []
+                    for river in rivers.geometry:
+                        if river.intersects(partitions[1][i]):
+                            partition_rivers.append(shapely.intersection(river, partitions[1][i]))
+                    self.__random_displacement(partition, partition_roads, partition_rivers)
         else:
-            buildings = self.__random_displacement(buildings, roads, rivers)
+            self.__random_displacement(buildings, roads, rivers)
 
-        return buildings
+        return self.__BUILDINGS
 
     def __random_displacement(self, buildings, roads, rivers):
         # Calculating the mean rate of overlapping buildings with other buildings, roads and rivers
@@ -68,22 +69,23 @@ class BuildingDisplacementRandom:
             if (self.__get_building_overlap(random_building, buildings, roads, rivers) != 0):
                 # Selecting a random angle (0-360)
                 random_angle = random.uniform(0, 360)
-                # Selecting a random lenght (0-max displacement variable)
+                # Selecting a random length (0-max displacement variable)
                 random_length = random.uniform(0, self.MAX_DISPLACEMENT)
                 # Calculate displacement for x and y
                 dx = math.cos(random_angle) * random_length
                 dy = math.sin(random_angle) * random_length
 
                 # Translating the building with the random values
-                translated = shapely.affinity.translate(random_building, dx, dy)
-                buildings[random_index] = translated
+                untranslated = self.__BUILDINGS.iloc[random_building].copy()
+                translated = shapely.affinity.translate(untranslated.geometry, dx, dy)
+                self.__BUILDINGS.loc[random_building, 'geometry'] = translated
 
                 # Calulcating the new rate mean
                 new_rate_mean = self.__get_buildings_overlapping_rate_mean(buildings, roads, rivers)
 
                 # If the new rate mean is equal or higher, we cancel the translation
                 if (new_rate_mean >= rate_mean):
-                    buildings[random_index] = random_building
+                    self.__BUILDINGS.loc[random_building, 'geometry'] = untranslated.geometry
                     trial += 1
                 # Else, resetting the trial count and updating the rate mean
                 else:
@@ -92,7 +94,6 @@ class BuildingDisplacementRandom:
                     if self.VERBOSE:
                         print("        {0}".format(rate_mean))
 
-        return buildings
 
     # Calculate the overlapping mean rate
     def __get_buildings_overlapping_rate_mean(self, buildings, roads, rivers):
@@ -116,20 +117,20 @@ class BuildingDisplacementRandom:
     # Calculate the area of overlapping between buildings, roads and rivers
     def __get_building_overlap(self, processed_building, buildings, roads, rivers):
         geometry = None
-
+        b = self.__BUILDINGS.iloc[processed_building].geometry
         # For each buildings...
         for building in buildings:
             # Checking if it's not the same building
             if building != processed_building:
-                geometry = self.__get_overlapping_geometries(processed_building, building, geometry, "building")
+                geometry = self.__get_overlapping_geometries(b, self.__BUILDINGS.iloc[building].geometry, geometry, "building")
         
         # For each road section
         for road in roads:
-            geometry = self.__get_overlapping_geometries(processed_building, road, geometry, "road")
+            geometry = self.__get_overlapping_geometries(b, road, geometry, "road")
 
         # For each river section
         for river in rivers:
-            geometry = self.__get_overlapping_geometries(processed_building, river, geometry, "river")
+            geometry = self.__get_overlapping_geometries(b, river, geometry, "river")
 
         # Returning the area of the geometry if it exists
         if (geometry is None) or (geometry.is_empty == True) or (geometry.area == 0):
