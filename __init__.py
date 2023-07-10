a811764496a202cbef958310cacb08e3372a0b8e
jberli <justinberli@gmail.com>
2023-07-10 13:54:19 +0200

    upgrade cartagen4py to 0.1.11 + wheel dist

--- __init__.py
+++ __init__.py
@@ -4,6 +4,6 @@ cartagen4py
 Python package to generalise geographic objects for cartographic purposes
 """
 
-__version__ = "0.1.10"
-__author__ = 'Guillaume Touya'
+__version__ = "0.1.11"
+__author__ = 'Guillaume Touya, Justin Berli'
 __credits__ = 'IGN'
