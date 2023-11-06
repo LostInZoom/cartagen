6a631c886449fef233538ebae667293fe08edb3f
jberli <justinberli@gmail.com>
2023-11-06 18:21:40 +0100

    graph creation init

---
+++ cartagen4py/utils/graph/roads.py
@@ -0,0 +1,7 @@
+import networkx as nx
+
+def create_road_graph(roads):
+    """
+    Create a networkx graph from a given list of roads linestrings.
+    The provided network must be fully connected.
+    """
