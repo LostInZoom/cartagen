83884245d3dc3e52b05822514e02fa92719c0f45
jberli <justinberli@gmail.com>
2024-03-05 16:28:29 +0100

    set default gaussian smoothing to densify line

--- cartagen4py/algorithms/lines/line_smoothing.py
+++ cartagen4py/algorithms/lines/line_smoothing.py
@@ -4,7 +4,7 @@ from shapely.geometry import LineString, Point
 from math import pi, sqrt, exp
 from cartagen4py.utils.geometry.line import densify_geometry, to_2d, get_index_of_nearest_vertex
 
-def gaussian_smoothing(line, sigma, threshold):
+def gaussian_smoothing(line, sigma, sample, densify=True):
     """
     Compute the gaussian smoothing of a set of a LineString.
     Parameters
@@ -13,19 +13,20 @@ def gaussian_smoothing(line, sigma, threshold):
         The line to smooth.
     sigma : float
         Gaussian filter strength.
-    threshold : int
-        Amount of nodes to add during the subsampling phase.
+    sample : int
+        The length in meter between each nodes after resampling the line.
+    densify : boolean optional
+        Whether the resulting line should keep the new node density. Default to True.
     """
-
     # First resample the line, making sure there is a maximum distance between two consecutive vertices
-    resampled = densify_geometry(line, threshold)
+    resampled = densify_geometry(line, sample)
 
     # Calculate the interval (number of vertex to take into consideration when smoothing)
-    interval = round(4 * sigma / threshold)
+    interval = round(4 * sigma / sample)
     # If the interval is longer than the input line, we change the interval and recalculate the sigma
     if interval >= len(resampled.coords):
         interval = len(resampled.coords) - 1
-        sigma = interval * threshold / 4
+        sigma = interval * sample / 4
     
     # Compute gaussian coefficients
     c2 = -1.0 / (2.0 * sigma * sigma)
@@ -53,34 +54,37 @@ def gaussian_smoothing(line, sigma, threshold):
             y += weights[abs(k)] * p1[1] / total
         smoothed_coords.append((x,y))
 
-    # Only return the points matching the input points in the resulting filtered line
-    final_coords = []
-    # Stores for index of already treated vertices
-    done = []
-    # Loop through initial vertices
-    for point in list(line.coords):
-        # Set the distance to infinite
-        distance = float("inf")
-        nearest = None
-        # Loop through smoothed coordinates
-        for i in range(len(smoothed_coords)):
-            # Check that the index has not been already added
-            if i not in done:
-                # Calculate distance from the point
-                d = Point(smoothed_coords[i]).distance(Point(point))
-                if d < distance:
-                    # Update distance and nearest index if below existing
-                    distance, nearest = d, i
+    if densify:
+        return LineString(smoothed_coords)
+    else:
+        # Only return the points matching the input points in the resulting filtered line
+        final_coords = []
+        # Stores for index of already treated vertices
+        done = []
+        # Loop through initial vertices
+        for point in list(line.coords):
+            # Set the distance to infinite
+            distance = float("inf")
+            nearest = None
+            # Loop through smoothed coordinates
+            for i in range(len(smoothed_coords)):
+                # Check that the index has not been already added
+                if i not in done:
+                    # Calculate distance from the point
+                    d = Point(smoothed_coords[i]).distance(Point(point))
+                    if d < distance:
+                        # Update distance and nearest index if below existing
+                        distance, nearest = d, i
 
-        # If a nearest point has been found, add it to the new line
-        if nearest is not None:
-            final_coords.append(smoothed_coords[nearest])
-            # Add the index as treated already
-            done.append(nearest)
-        else:
-            final_coords.append(point)
+            # If a nearest point has been found, add it to the new line
+            if nearest is not None:
+                final_coords.append(smoothed_coords[nearest])
+                # Add the index as treated already
+                done.append(nearest)
+            else:
+                final_coords.append(point)
 
-    return LineString(final_coords)
+        return LineString(final_coords)
 
 # Extend the given set of points at its first and last points of k points using central inversion.
 def __extend(line, interval):
