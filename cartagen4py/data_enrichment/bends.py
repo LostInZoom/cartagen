f5e0b230e05b12130dfa61aedff95b9b6fca8500
jberli <justinberli@gmail.com>
2024-03-05 14:50:17 +0100

    added inflexion points calculation

--- cartagen4py/data_enrichment/bends.py
+++ cartagen4py/data_enrichment/bends.py
@@ -2,12 +2,22 @@ import geopandas as gpd
 import shapely
 
 from cartagen4py.algorithms.lines.line_smoothing import *
+from cartagen4py.utils.geometry.angle import *
+from cartagen4py.utils.geometry.line import *
+
+# from test_functions import *
 
 def detect_bends(line, sigma, threshold):
     """
     Detect bends from a given LineString.
     """
 
+    # Create the gaussian smoothed line
     smoothed = gaussian_smoothing(line, sigma, threshold)
+
+    # Get inflexion points indexes
+    indexes = inflexion_points(smoothed)
+    
+    # make_gdf([{'geometry': shapely.Point(list(smoothed.coords)[x])} for x in indexes], 'inflexion')
 
     return smoothed
