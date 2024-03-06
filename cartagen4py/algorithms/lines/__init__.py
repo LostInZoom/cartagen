288f96204b8d074f18778ed1bc6c88fc062bd49f
jberli <justinberli@gmail.com>
2024-03-06 10:50:00 +0100

    accordion has bend algo

--- cartagen4py/algorithms/lines/__init__.py
+++ cartagen4py/algorithms/lines/__init__.py
@@ -1,3 +1,4 @@
+from cartagen4py.algorithms.lines.bends import *
 from cartagen4py.algorithms.lines.breaks import *
 from cartagen4py.algorithms.lines.line_simplification import *
 from cartagen4py.algorithms.lines.line_smoothing import *
---
+++ cartagen4py/algorithms/lines/bends.py
@@ -0,0 +1,17 @@
+import shapely
+
+from cartagen4py.algorithms.lines.line_smoothing import *
+from cartagen4py.utils.geometry.bends import *
+
+from test_functions import *
+
+def accordion(line, sigma, sample):
+    """
+    Apply the accordion algorithm to a linestring.
+    """
+
+    # Smooth the line to avoid unnecessary micro inflexion points
+    smoothed = gaussian_smoothing(line, sigma, sample, densify=False)
+
+    # Detect individual bends inside the smoothed line
+    bs = BendSerie(smoothed)
