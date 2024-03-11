1cb076a34a6f71b34876eafcc3dd8ca538fb7718
jberli <justinberli@gmail.com>
2024-03-11 10:26:10 +0100

    calculate bends parameters

--- cartagen4py/utils/geometry/bends.py
+++ cartagen4py/utils/geometry/bends.py
@@ -1,4 +1,7 @@
-import shapely
+import numpy as np
+
+from shapely import Point, LineString
+from shapely.ops import nearest_points
 
 from cartagen4py.utils.geometry.angle import *
 from cartagen4py.utils.geometry.line import *
@@ -8,26 +11,56 @@ class Bend:
     Create a bend object from a given linestring.
     """
     def __init__(self, line):
+        self.bend = line
         self.side = get_bend_side(line)
 
         # Get the bend line as a vertex list
-        coords = list(line.coords)
+        coords = list(self.bend.coords)
+
         # Calculate the base segment
-        base = shapely.LineString([coords[0], coords[-1]])
+        base = LineString([coords[0], coords[-1]])
 
-        # Calculate the middle of the base segment
-        middle = base.interpolate(base.length / 2)
+        self.width = base.length
 
-        summit = None
-        distmax = 0
-        for i in range(1, len(coords) - 2):
-            point = shapely.Point(coords[i])
-            dist = point.distance(middle)
-            if dist > distmax:
-                summit = point
-                distmax = dist
+        # Calculate the base point of the base segment
+        self.middle = base.interpolate(base.length / 2)
 
-        self.summit = summit
+        # Smooth the bend
+        smoothed = gaussian_smoothing(self.bend, densify=False)
+        # Get the vertices list
+        scoords = list(smoothed.coords)
+
+        curvature_max = 0
+        smooth_summit = None
+        # Loop through vertices without first and last
+        for i in range(1, len(scoords) - 2):
+            # Get the previous, current and next vertex
+            p1, p2, p3 = Point(scoords[i - 1]), Point(scoords[i]), Point(scoords[i + 1])
+            # Calculate the curvature
+            curvature = get_curvature(p1, p2, p3)
+            # Update the max curvature if above
+            if curvature > curvature_max:
+                curvature_max = curvature
+                smooth_summit = p2
+
+        # Set the distance to infinite
+        distance = float("inf")
+        for c in coords:
+            d = Point(c).distance(smooth_summit)
+            if d < distance:
+                # Update distance and nearest index if below existing
+                distance = d
+                self.summit = Point(c)
+
+        # Calculate the orientation of the bend
+        orientation = np.arctan2(self.summit.y - self.middle.y, self.summit.x - self.middle.x)
+
+        # Set the orientation between 0 and 2pi
+        modulo = orientation % (2 * np.pi)
+        if modulo >= 0:
+            self.orientation = abs(modulo)
+        else:
+            self.orientation = modulo + 2 * np.pi
 
 class BendSerie:
     """
@@ -53,9 +86,9 @@ class BendSerie:
             # If the vertex is an inflexion point
             if i in inflexion:
                 # Add the bend to the list
-                self.bends.append(Bend(shapely.LineString(bend)))
+                self.bends.append(Bend(LineString(bend)))
                 # Restart a new bend
                 bend = [vertex]
 
         # Add the last bend to the list
-        self.bends.append(Bend(shapely.LineString(bend)))
+        self.bends.append(Bend(LineString(bend)))
