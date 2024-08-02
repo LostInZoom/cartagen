819c9824c00dd4c28b947c5449dacb169323dd63
jberli <justinberli@gmail.com>
2024-08-02 15:21:53 +0200

    v0.3.6 upload

--- __init__.py
+++ __init__.py
@@ -4,6 +4,6 @@ cartagen4py
 Python package to generalise geographic objects for cartographic purposes
 """
 
-__version__ = '0.3.5'
+__version__ = '0.3.6'
 __author__ = 'Guillaume Touya, Justin Berli, Azelle Courtial'
 __credits__ = 'IGN'
