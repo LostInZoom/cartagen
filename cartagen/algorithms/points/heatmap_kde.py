251819cbe33520d0a678cab29e3ccfb50a4a82ea
gowestmen <paulbourcier86@gmail.com>
2024-08-30 15:30:46 +0200

    adding heatmap_kde v1

---
+++ cartagen/algorithms/points/heatmap_kde.py
@@ -0,0 +1,93 @@
+import geopandas as gpd
+import numpy as np
+from shapely.geometry import Polygon
+
+def heatmap_kde(points, cell_size, radius, value=None, function='quartic'):
+    """
+    Perform spatial smoothing with the kernel density estimation technique (KDE), 
+    also known as heatmap. This code is partially based on this script `<https://niitdigital.medium.com/how-to-create-a-heatmap-from-scratch-in-python-234f602e856e>`
+
+    For more information about KDE,
+    here is a link to the related `wikipedia article
+    <https://en.wikipedia.org/wiki/Kernel_density_estimation>`_
+
+    Parameters
+    ----------
+    points : GeoDataFrame of Point
+        The points used to calculates density. Has to be in projected CRS.
+    cell_size : int
+      The size of the cell of the grid containing density values. Small size means smoother result,
+      but also higher computation time.
+    radius : int
+      The radius used for the density calculation in each grid cells. 
+      For each centroid of grid cell, all the points within the radius are taken in account for density calculation 
+      Big radius means more generalized results.
+    value : str, optionnal 
+      The name of the attribute value of the point to use to weight the density value.
+    function : str, optionnal
+      The name of the smoothing function that calculates the density value of each point within the radius.
+      Each function impacts the way distance is important in the density calculation.
+      Default to 'quartic'. 
+    Returns
+    -------
+    GeoDataFrame of polygons
+    The grid containing the values of density
+
+    See Also
+    --------
+    reduce_quadtree :
+        Reduce a set of points using a quadtree.
+    reduce_labelgrid :
+        Reduce a set of points using the Label Grid method.
+    """
+    #create square grid 
+    xmin = points.total_bounds[0]
+    xmax = points.total_bounds[2]
+    ymin = points.total_bounds[1]
+    ymax = points.total_bounds[3]
+
+
+    cols = list(np.arange(xmin, xmax + cell_size, cell_size))
+    rows = list(np.arange(ymin, ymax + cell_size, cell_size))
+            
+    polygons = gpd.GeoSeries([Polygon([(x, y), (x + cell_size, y), (x + cell_size, y + cell_size), 
+                        (x, y + cell_size)]) for x in cols[:-1] for y in rows[:-1]])
+
+    #retrieving grid cells centroid
+    centroids = polygons.centroid                
+    
+    #retrieving value attribute and store in a list
+    if value is not None:
+        lst_values = list(points[value])
+
+    #storing x and y value of points and centroid into lists
+    centroids_x = list(centroids.x)
+    centroids_y = list(centroids.y)
+    points_x = list(points.geometry.x)
+    points_y = list(points.geometry.y)
+
+    #definition de la fonction de lissage par méthode des noyaux (quadratique)
+    if function == 'quartic':
+        def kernel(d,radius):
+            dn=d/radius
+            P=(15/16)*(1-dn**2)**2
+            return P
+
+    #density calculation
+    intensity_list = [] # list to store density value of each cell
+    for i in range(len(centroids)): 
+        kde_value_list = []
+        for k in range(len(points_x)):
+            d = np.sqrt((centroids_x[i]-points_x[k])**2+(centroids_y[i]-points_y[k])**2)
+            if d <= radius:
+                p= kernel(d,radius)
+                if value is None:
+                    kde_value_list.append(p)
+                else: 
+                    kde_value_list.append(p*lst_values[k])
+
+        total_density = sum(kde_value_list)
+        intensity_list.append(total_density)
+   
+    final_grid = gpd.GeoDataFrame({'geometry':polygons,'density':intensity_list})
+    return final_grid
