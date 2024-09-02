aa2a53e1b591837904dda3865823dbb38d9ce799
jberli <justinberli@gmail.com>
2024-09-02 11:52:45 +0200

    change heatmap import

--- cartagen/algorithms/points/__init__.py
+++ cartagen/algorithms/points/__init__.py
@@ -1,3 +1,3 @@
 from cartagen.algorithms.points.covering import hull_delaunay, hull_swinging_arm
 from cartagen.algorithms.points.reduction import reduce_kmeans, reduce_quadtree, reduce_labelgrid
-from cartagen.algorithms.points.heatmap_kde import heatmap
+from cartagen.algorithms.points.heatmap import heatmap
