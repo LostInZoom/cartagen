b5ec2361fa46bdf5adea3b2b3db51cbaff011bd7
jberli <justinberli@gmail.com>
2024-09-13 16:21:10 +0200

    removed vscode git nonsense

--- cartagen/enrichment/network/__init__.py
+++ cartagen/enrichment/network/__init__.py
@@ -1,10 +1,6 @@
 from cartagen.enrichment.network.branching_crossroads import detect_branching_crossroads, is_branching_crossroad
 from cartagen.enrichment.network.dead_ends import detect_dead_ends
-<<<<<<< HEAD
 from cartagen.enrichment.network.dual_carriageways import detect_dual_carriageways, is_dual_carriageway
-=======
-from cartagen.enrichment.network.dual_carriageways import detect_dual_carriageways
->>>>>>> d88484630a57a77207ddd284389f33a7af9c63f9
 from cartagen.enrichment.network.roundabouts import detect_roundabouts, is_roundabout
 from cartagen.enrichment.network.rural_areas import rural_betweeness, rural_traffic
 from cartagen.enrichment.network.strokes import strokes_roads
