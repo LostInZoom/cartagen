acfcd51d97d4cf857bde03926c3765346cfe75ec
Justin Berli <justinberli@gmail.com>
2026-02-25 15:18:50 +0100

    optimized group intersection function

--- cartagen/utils/geometry/distances.py
+++ cartagen/utils/geometry/distances.py
@@ -16,24 +16,65 @@ def distance_area(geom1, geom2):
     else:
         return None
 
-def group_intersecting(geoms):
+# def group_intersecting(geoms):
+#     """
+#     Group intersecting geometries in nested lists.
+#     """
+#     done = []
+#     groups = []
+#     groupsgeom = []
+#     for i, p1 in enumerate(geoms):
+#         if i not in done:
+#             done.append(i)
+#             group = [ i ]
+#             groupgeom = p1
+#             for j, p2 in enumerate(geoms):
+#                 if j not in done:
+#                     if shapely.intersects(groupgeom, p2):
+#                         done.append(j)
+#                         group.append(j)
+#                         groupgeom = shapely.union(groupgeom, p2)
+#             groups.append(group)
+#             groupsgeom.append(groupgeom)
+#     return groups
+
+def group_intersecting(polygons):
     """
     Group intersecting geometries in nested lists.
     """
-    done = []
-    groups = []
-    groupsgeom = []
-    for i, p1 in enumerate(geoms):
-        if i not in done:
-            done.append(i)
-            group = [ i ]
-            groupgeom = p1
-            for j, p2 in enumerate(geoms):
-                if j not in done:
-                    if shapely.intersects(groupgeom, p2):
-                        done.append(j)
-                        group.append(j)
-                        groupgeom = shapely.union(groupgeom, p2)
-            groups.append(group)
-            groupsgeom.append(groupgeom)
-    return groups
+    n = len(polygons)
+    parent = list(range(n))
+    
+    def find(x):
+        if parent[x] != x:
+            parent[x] = find(parent[x])
+        return parent[x]
+    
+    def union(x, y):
+        root_x = find(x)
+        root_y = find(y)
+        if root_x != root_y:
+            parent[root_x] = root_y
+    
+    # Create a spatial index
+    tree = shapely.STRtree(polygons)
+    
+    # For each polygon, find candidates for intersection
+    for i, poly in enumerate(polygons):
+        # Spatial request
+        candidats_idx = tree.query(poly)
+        
+        for j in candidats_idx:
+            # Union only if current polygon index is below candidate
+            if i < j and polygons[i].intersects(polygons[j]):
+                union(i, j)
+    
+    # Regroup indexes
+    groups = {}
+    for i in range(n):
+        root = find(i)
+        if root not in groups:
+            groups[root] = []
+        groups[root].append(i)
+    
+    return list(groups.values())
