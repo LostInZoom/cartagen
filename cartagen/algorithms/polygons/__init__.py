0c480b5dd966a16091874117b9c0e18362f58aba
Justin Berli <justinberli@gmail.com>
2026-04-16 19:19:33 +0200

    added area patche generalisation

--- cartagen/algorithms/polygons/__init__.py
+++ cartagen/algorithms/polygons/__init__.py
@@ -2,3 +2,4 @@ from cartagen.algorithms.polygons.boundaries import (
     boundaries_douglas_peucker, boundaries_visvalingam_whyatt,
     boundaries_raposo, boundaries_li_openshaw
 )
+from cartagen.algorithms.polygons.patches import generalise_area_patches
