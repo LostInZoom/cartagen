7fa030fd74043ff99a18ea95c332ffd360fda245
Justin Berli <justinberli@gmail.com>
2026-04-16 19:19:09 +0200

    added wma line smoothing

--- cartagen/utils/lines/smoothing/__init__.py
+++ cartagen/utils/lines/smoothing/__init__.py
@@ -3,3 +3,4 @@ from cartagen.utils.lines.smoothing.chaikin import smooth_chaikin
 from cartagen.utils.lines.smoothing.gaussian import smooth_gaussian
 from cartagen.utils.lines.smoothing.platre import smooth_platre
 from cartagen.utils.lines.smoothing.taubin import smooth_taubin
+from cartagen.utils.lines.smoothing.wma import smooth_wma
