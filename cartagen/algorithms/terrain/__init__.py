38a83953d8bcc91ed6446ad656a787664e8b22d1
gtouya <guillaume.touya@ign.fr>
2026-09-04 13:20:34 +0200

    Update __init__.py

--- cartagen/algorithms/terrain/__init__.py
+++ cartagen/algorithms/terrain/__init__.py
@@ -1,3 +1,3 @@
 from cartagen.algorithms.terrain.terrain_sculptor import *
 from cartagen.algorithms.terrain.DTM_filtering import *
-from cartagen.algorithms.terrain.skeleton_based_methods import *
+#from cartagen.algorithms.terrain.skeleton_based_methods import *
